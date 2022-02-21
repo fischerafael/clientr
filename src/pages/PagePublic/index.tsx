a7d1749f13cf7c62a8c0384c130a68402524a2dc
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-02-21 20:28:13 -0300

    refactor: extracted left section to component

--- src/pages/PagePublic/index.tsx
+++ src/pages/PagePublic/index.tsx
@@ -19,33 +19,38 @@ export const PagePublic = () => {
         justify="space-between"
         align="center"
       >
-        <VStack
-          w="full"
-          maxW="container.sm"
-          h="full"
-          justify="center"
-          align="flex-start"
-          spacing="8"
-        >
-          <Text fontWeight="bold" color="pink.500" fontSize="xl">
-            Clientr
-          </Text>
-
-          <Text fontSize="6xl" fontWeight="bold" lineHeight="1">
-            Seu sistema de gerenciamento de clientes na nuvem.
-          </Text>
-
-          <Text fontSize="lg">
-            Gerencie seus clientes de uma maneira fácil, rápida e centralizada.
-          </Text>
-
-          <Button colorScheme="pink">Acessar Conta</Button>
-        </VStack>
-
+        <LeftSection />
         <VStack maxW="container.sm">
           <Image objectFit="contain" src="/hero-image.svg" />
         </VStack>
       </Flex>
     </Flex>
+  );
+};
+
+const LeftSection = () => {
+  return (
+    <VStack
+      w="full"
+      maxW="container.sm"
+      h="full"
+      justify="center"
+      align="flex-start"
+      spacing="8"
+    >
+      <Text fontWeight="bold" color="pink.500" fontSize="xl">
+        Clientr
+      </Text>
+
+      <Text fontSize="6xl" fontWeight="bold" lineHeight="1">
+        Seu sistema de gerenciamento de clientes na nuvem.
+      </Text>
+
+      <Text fontSize="lg">
+        Gerencie seus clientes de uma maneira fácil, rápida e centralizada.
+      </Text>
+
+      <Button colorScheme="pink">Acessar Conta</Button>
+    </VStack>
   );
 };
