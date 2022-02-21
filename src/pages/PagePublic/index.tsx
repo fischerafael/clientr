3629e1c2e112074e8a9e8569b33bd2e392867b5e
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-02-21 20:42:32 -0300

    refactor: change public page for a template

--- src/pages/PagePublic/index.tsx
+++ src/pages/PagePublic/index.tsx
@@ -1,32 +1,8 @@
 import React from "react";
-import { Flex } from "@chakra-ui/react";
 
-import { LeftSection } from "./components/LeftSection";
-import { RightSection } from "../../components/Organisms/RightSection";
 import { TemplatePagePublic } from "../../components/Templates/TemplatePublicPage";
+import { LeftSection } from "./components/LeftSection";
 
 export const PagePublic = () => {
-  return (
-    <TemplatePagePublic leftSection={<LeftSection />} />
-    // <Flex
-    //   w="full"
-    //   h="100vh"
-    //   bg="purple.50"
-    //   px="8"
-    //   fontFamily="sans-serif"
-    //   color="pink.900"
-    //   justify="center"
-    // >
-    //   <Flex
-    //     w="full"
-    //     h="full"
-    //     maxW="container.xl"
-    //     justify="space-between"
-    //     align="center"
-    //   >
-    //     <LeftSection />
-    //     <RightSection />
-    //   </Flex>
-    // </Flex>
-  );
+  return <TemplatePagePublic leftSection={<LeftSection />} />;
 };
