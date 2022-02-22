49b8e2976e35a60672ffeb536a90ca011f6caccb
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-02-22 17:31:21 -0300

    feat: add create account page

---
+++ pages/create-account/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { PagePublicCreateAccount } from "../../src/pages/PagePublicCreateAccount";
+
+const index = () => {
+  return <PagePublicCreateAccount />;
+};
+
+export default index;
