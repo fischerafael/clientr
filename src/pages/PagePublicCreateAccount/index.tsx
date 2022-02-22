e91443c3ff3bdd40a18b00f78c6e28aa283bbf77
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-02-22 17:31:21 -0300

    feat: add create account page

---
+++ src/pages/PagePublicCreateAccount/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import { Button, HStack, Input, Text, VStack } from "@chakra-ui/react";
+
+import { TemplatePagePublic } from "../../components/Templates/TemplatePublicPage";
+import { NextLink } from "../../components/Atoms/NextLink";
+
+export const PagePublicCreateAccount = () => {
+  return (
+    <TemplatePagePublic
+      leftSection={
+        <VStack w="full" align="flex-start" spacing="8">
+          <Text fontWeight="bold" color="pink.500" fontSize="xl">
+            Criar Conta
+          </Text>
+
+          <Text>Crie sua conta</Text>
+
+          <Input bg="white" placeholder="Nome" />
+          <Input bg="white" placeholder="Email" />
+
+          <Button alignSelf="flex-end" colorScheme="pink">
+            Criar Conta
+          </Button>
+
+          <HStack w="full" justify="space-between">
+            <NextLink href="/create-account" label="Criar Conta" />
+            <NextLink href="/" label="Voltar" />
+          </HStack>
+        </VStack>
+      }
+    />
+  );
+};
